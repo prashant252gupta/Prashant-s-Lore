a36f9290cf4556e476e5b1bea88b52305296be6c
Prashant Gupta <pguptaprashant252@gmail.com>
2025-08-11 12:04:51 +0000

    i want no layer on top of the animation, only the spline animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-import SplineBackground from '@/components/spline-background';
-import Header from '@/components/header';
-import Hero from '@/components/hero';
-import Projects from '@/components/projects';
-import About from '@/components/about';
-import Blog from '@/components/blog';
-import Contact from '@/components/contact';
-import Footer from '@/components/footer';
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col min-h-screen">
-      <SplineBackground />
-      <Header />
-      <main className="flex-grow">
-        <div id="home" className="pt-16 sm:pt-24"><Hero /></div>
-        <div id="projects" className="pt-16 sm:pt-24"><Projects /></div>
-        <div id="about" className="pt-16 sm:pt-24"><About /></div>
-        <div id="blog" className="pt-16 sm:pt-24"><Blog /></div>
-        <div id="contact" className="pt-16 sm:pt-24"><Contact /></div>
-      </main>
-      <Footer />
+    <div className="w-full h-screen">
+      <Spline scene="https://prod.spline.design/1zeQtflKZD13UN8W/scene.splinecode" />
     </div>
   );
 }
