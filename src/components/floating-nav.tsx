6cd2ef36bcf265bfffb8df0e4f87cfa1fdcd16ff
Prashant Gupta <pguptaprashant252@gmail.com>
2025-08-24 18:05:30 +0000

    make the project bubble gundam.png circular

--- src/components/floating-nav.tsx
+++ src/components/floating-nav.tsx
@@ -35,7 +35,7 @@ const navItems = [
         alt="Projects"
         width={40}
         height={40}
-        className="object-contain"
+        className="object-contain rounded-full"
         data-ai-hint="robot"
       />
     ),
